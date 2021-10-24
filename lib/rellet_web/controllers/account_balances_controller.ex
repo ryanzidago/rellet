b5fd66d962b583e1b939b0dae78f03bbe8d3d461
Ryan Zidago <ryanzidago@gmail.com>
2021-10-24 22:56:25 +0200

    cleaned up code

--- lib/rellet_web/controllers/account_balances_controller.ex
+++ lib/rellet_web/controllers/account_balances_controller.ex
@@ -1,10 +1,10 @@
 defmodule RelletWeb.AccountBalancesController do
   use RelletWeb, :controller
 
-  alias Rellet.Accounts
+  alias Rellet.Accounts.Account
 
   def show(conn, %{"account_id" => account_id}) do
-    account_balances = Accounts.get_account_balances_by_account_id(account_id)
+    account_balances = Account.Balances.get_by_account_id(account_id)
     json(conn, account_balances)
   end
 end
