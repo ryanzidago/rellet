920b36d2aaa0905155a86f2b3729cb018729e944
Ryan Zidago <ryanzidago@gmail.com>
2021-10-24 21:19:22 +0200

    cleaned up code

--- lib/rellet_web/controllers/account_transaction_controller.ex
+++ lib/rellet_web/controllers/account_transaction_controller.ex
@@ -5,7 +5,6 @@ defmodule RelletWeb.AccountTransactionController do
 
   def index(conn, %{"account_id" => account_id}) do
     account_transactions = Accounts.get_all_account_transactions_by_account_id(account_id)
-    ids = account_transactions |> Enum.map(& &1.id)
     json(conn, account_transactions)
   end
 
